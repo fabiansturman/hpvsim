1553c0a15b7b9854189c44aaa98db1a8a5c40c55
fabiansturman <60733643+fabiansturman@users.noreply.github.com>
2024-01-05 20:08:50 +0000

    practicing calibration

--- examples/Fabian Testing HPVsim informally/calibration.py
+++ examples/Fabian Testing HPVsim informally/calibration.py
@@ -4,7 +4,7 @@ import hpvsim as hpv
 if __name__ == "__main__":#to allow for using several workers in parellell
 
     # Configure a simulation with some parameters
-    pars = dict(n_agents=10e3, start=1980, end=2020, dt=0.25, location='nigeria')
+    pars = dict(n_agents=10e3, start=1970, end=2020, dt=0.25, location='nigeria')
     sim = hpv.Sim(pars)
 
     # Specify some parameters to adjust during calibration.
@@ -41,9 +41,9 @@ if __name__ == "__main__":#to allow for using several workers in parellell
         genotype_pars=genotype_pars,
         extra_sim_result_keys=results_to_plot,
         datafiles=datafiles,
-        total_trials=700, n_workers=9, 
+        total_trials=7000, n_workers=12, 
         keep_db=True, #for some reason there is a bug where if i set keep_db to its default value of false, i get a WinError 32 (the process cannot access the file because it is being used by another process) relating to line 431 of calibration.py
-        name="hpvsim_calubration_6" #to keep track of my calibrations
+        name="hpvsim_calubration_7000iters" #to keep track of my calibrations
     )
     calib.calibrate(die=True)
     calib.plot(res_to_plot=4);
